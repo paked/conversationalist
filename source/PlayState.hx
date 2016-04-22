0a5ff8463366e8ee8c0a3a3231d79c09390c0eea
Harrison Shoebridge <harrison@theshoebridges.com>
2016-04-22 21:19:42 +1000

    Add basic text navigation

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,9 +1,14 @@
 package;
 
+import haxe.Json;
+
+import openfl.Assets;
+
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
+import flixel.group.FlxGroup;
 import flixel.ui.FlxButton;
 import flixel.util.FlxColor;
 import flixel.math.FlxMath;
@@ -14,20 +19,98 @@ class PlayState extends FlxState {
 
   private var text: FlxText;
 
+  private var options: FlxTypedGroup<FlxText>;
+
+  private var story: Map<String, Block>;
+  private var current: Block;
+
+  private var selected = 0;
+
 	override public function create(): Void {
+		super.create();
+
     FlxG.camera.bgColor = babyBlue;
 
-    text = new FlxText(0, FlxG.height/3, FlxG.width, "Hello", 32);
+    var rawStory: Story = Json.parse(Assets.getText("assets/data/story.json"));
+    trace(rawStory.story[0]);
+  
+    story = new Map<String, Block>();
+    for (block in rawStory.story) {
+      story.set(block.name, block);
+    }
+ 
+    options = new FlxTypedGroup<FlxText>();
+
+    for (i in 0...10) {
+      var txt = new FlxText(0, FlxG.height - FlxG.height/4, FlxG.width, "", 32);
+      txt.exists = true;
+
+      options.add(txt);
+    }
+
+    add(options);
+
+    text = new FlxText(0, FlxG.height/3, FlxG.width, "", 32);
     text.alignment = FlxTextAlign.CENTER;
     text.addFormat(new FlxTextFormat(niceRed));
-    text.text = "Fuck.";
 
     add(text);
 
-		super.create();
-	}
+    changeBlock();
+ 	}
 
 	override public function update(elapsed: Float): Void {
 		super.update(elapsed);
+
+    if (FlxG.keys.justPressed.LEFT) {
+      selected -= 1;
+    } else if (FlxG.keys.justPressed.RIGHT) {
+      selected += 1;
+    }
+
+    if (FlxG.keys.justPressed.ENTER) {
+      var finalSelected = selected % current.links.length;
+      trace(finalSelected);
+
+      changeBlock(current.links[finalSelected].to);
+    }
 	}
+
+  private function changeBlock(key: String = "start") {
+    current = story.get(key);
+
+    options.forEachExists(function(option: FlxText) {
+        option.exists = false;
+      });
+
+    var optionWidth = FlxG.width / current.links.length;
+
+    for (i in 0...current.links.length) {
+      var link = current.links[i];
+
+      var option = options.getFirstAvailable();
+      option.width = optionWidth;
+      option.x = optionWidth * i;
+      option.text = link.name;
+      option.alignment = FlxTextAlign.CENTER;
+      option.exists = true;
+    }
+
+    text.text = current.text.join("\n");
+  }
+}
+
+typedef Story = {
+  story: Array<Block>
+};
+
+typedef Block = {
+  name: String,
+  text: Array<String>,
+  links: Array<Link>
+};
+
+typedef Link = {
+  name: String,
+  to: String
 }
