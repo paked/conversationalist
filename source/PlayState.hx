a6e1288c7ef94cb12831f7a1743318ab715ac0ed
Harrison Shoebridge <harrison@theshoebridges.com>
2016-04-22 19:59:54 +1000

    Fix line endings

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,17 +5,29 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
 import flixel.math.FlxMath;
 
-class PlayState extends FlxState
-{
-	override public function create():Void
-	{
+class PlayState extends FlxState {
+  private var babyBlue = FlxColor.fromString("#ABE5F5");
+  private var niceRed = FlxColor.fromString("#ED2898");
+
+  private var text: FlxText;
+
+	override public function create(): Void {
+    FlxG.camera.bgColor = babyBlue;
+
+    text = new FlxText(0, FlxG.height/3, FlxG.width, "Hello", 32);
+    text.alignment = FlxTextAlign.CENTER;
+    text.addFormat(new FlxTextFormat(niceRed));
+    text.text = "Fuck.";
+
+    add(text);
+
 		super.create();
 	}
 
-	override public function update(elapsed:Float):Void
-	{
+	override public function update(elapsed: Float): Void {
 		super.update(elapsed);
 	}
 }
